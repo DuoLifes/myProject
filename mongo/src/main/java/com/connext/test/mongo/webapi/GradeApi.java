6e28fccf4fab33d10f5b7f6532e2642ee45bf891
Duo <1564900966@qq.com>
2018-04-19 10:18:10 +0800

    字符集转换

--- mongo/src/main/java/com/connext/test/mongo/webapi/GradeApi.java
+++ mongo/src/main/java/com/connext/test/mongo/webapi/GradeApi.java
@@ -5,6 +5,8 @@ import com.connext.test.mongo.service.GradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.List;
 
 @RestController
@@ -21,5 +23,31 @@ public class GradeApi {
     @PostMapping
     public Grade addGrade(@RequestBody Grade grade){
         return gradeService.addGrade(grade);
+    }
+
+    @GetMapping("/test")
+    public String test(@RequestParam String data){
+        return data;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        // 将application/x-www-form-urlencoded字符串转换成普通字符串
+        // 其中的字符串直接从上图所示窗口复制过来,chrome 默认用 UTF-8 字符集进行编码，所以也应该用对应的字符集解码
+//        System.out.println("采用UTF-8字符集进行解码:");
+//        String keyWord = URLDecoder.decode("%E5%A4%A9%E6%B4%A5%E5%A4%A7%E5%AD%A6+Rico", "UTF-8");
+//        System.out.println(keyWord);
+//        System.out.println("\n 采用GBK字符集进行解码:");
+//        System.out.println(URLDecoder.decode("%E5%A4%A9%E6%B4%A5%E5%A4%A7%E5%AD%A6+Rico", "GBK"));
+
+        // 将普通字符串转换成application/x-www-form-urlencoded字符串
+        System.out.println("\n 采用utf-8字符集:");
+        String urlStr = URLEncoder.encode("天津大学", "utf-8");
+        System.out.println(urlStr);
+        String test=URLDecoder.decode("%E5%A4%A9%E6%B4%A5%E5%A4%A7%E5%AD%A6","UTF-8");
+        System.out.println(test);
+//        System.out.println("\n 采用GBK字符集:");
+//        String urlStr2 = URLEncoder.encode("天津大学", "GBK");
+//        System.out.println(urlStr2);
     }
 }
