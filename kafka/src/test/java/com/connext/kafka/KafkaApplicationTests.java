f72efcf5eebcea6479c1fa84fbb4bd8989ba5bbd
Duo <1564900966@qq.com>
2018-04-26 09:41:21 +0800

    kafka

--- kafka/src/test/java/com/connext/kafka/KafkaApplicationTests.java
+++ kafka/src/test/java/com/connext/kafka/KafkaApplicationTests.java
@@ -7,16 +7,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KafkaApplicationTests {
 
-	@Autowired
-	private KafkaSender kafkaSender;
-	@Test
-	public void contextLoads() {
-		//kafkaSender.send();
-		kafkaSender.mySend();
-	}
+    @Autowired
+    private KafkaSender kafkaSender;
 
+    @Test
+    public void contextLoads() {
+        //kafkaSender.send();
+        //kafkaSender.mySend();
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date one;
+        Date two;
+        long day = 0;
+        long hour = 0;
+        long min = 0;
+        long sec = 0;
+        String str1 = "2018-01-01 00:00:00";
+        String str2 = "2018-01-01 00:00:12";
+        try {
+            one = df.parse(str1);
+            two = df.parse(str2);
+            long time1 = one.getTime();
+            long time2 = two.getTime();
+            long diff;
+            if (time1 < time2) {
+                diff = time2 - time1;
+            } else {
+                diff = time1 - time2;
+            }
+            day = diff / (24 * 60 * 60 * 1000);
+            hour = (diff / (60 * 60 * 1000) - day * 24);
+            min = ((diff / (60 * 1000)) - day * 24 * 60 - hour * 60);
+            sec = (diff / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sec+"==============");
+    }
 }
+
+
