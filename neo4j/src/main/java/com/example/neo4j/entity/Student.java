2bfcf07de49b012a9e7f95f84fd399f2798b1587
Duo <1564900966@qq.com>
2018-05-08 13:54:00 +0800

    neo4j

---
+++ neo4j/src/main/java/com/example/neo4j/entity/Student.java
@@ -0,0 +1,45 @@
+package com.example.neo4j.entity;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+
+@NodeEntity
+public class Student {
+
+    @GraphId
+    private Long id;
+    @Property
+    private String name;
+    @Property
+    private String sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Student (String name, String sex){
+        this.name=name;
+        this.sex=sex;
+    }
+}
---
+++ neo4j/src/test/java/com/example/neo4j/Neo4jApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.neo4j;
+
+import com.example.neo4j.entity.Student;
+import com.example.neo4j.repository.StudentRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Neo4jApplicationTests {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	@Test
+	public void contextLoads() {
+		Student student=new Student("李四","女");
+		studentRepository.save(student);
+	}
+
+}
