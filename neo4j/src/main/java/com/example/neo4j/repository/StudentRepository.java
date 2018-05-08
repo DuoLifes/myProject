0db3fbb730f5775aec3f5ececb155f20a69da984
Duo <1564900966@qq.com>
2018-05-08 17:35:37 +0800

    neo4j

--- neo4j/src/main/java/com/example/neo4j/repository/StudentRepository.java
+++ neo4j/src/main/java/com/example/neo4j/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.example.neo4j.entity.Student;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface StudentRepository extends Neo4jRepository<Student,String> {
+
 }
